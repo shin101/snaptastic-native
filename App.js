32943f05521c947510a6f63d2af10da2a2edbf60
shin101 <kendras0127@gmail.com>
2023-07-09 14:45:47 +0900

    AppLoading

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const preload = async () => {
+      try {
+        await Font.loadAsync({ Ionicons: Ionicons.font });
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+          setLoading(false);
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+    preload();
+  }, []);
+
+  if (loading) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hihihihi Bacon</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
