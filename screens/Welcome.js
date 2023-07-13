ed7f43f74a8e05f9588b1d1aa90aba6896384eee
shin101 <kendras0127@gmail.com>
2023-07-13 18:29:35 +0900

    Welcome Screen

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,20 +1,50 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+`;
+
+const Logo = styled.Image`
+  max-width: 50%;
+  height: 100px;
+`;
+
+const CreateAccount = styled.View`
+  background-color: ${colors.blue};
+  padding: 7px 10px;
+  border-radius: 5px;
+`;
+const CreateAccountText = styled.Text`
+  color: white;
+  font-weight: 600;
+`;
+
+const LoginLink = styled.Text`
+  color: ${colors.blue};
+  font-weight: 600;
+  margin-top: 10px;
+`;
 
 export default function Welcome({ navigation }) {
+  const goToCreateAccount = () => navigation.navigate("CreateAccount");
+  const goToLogIn = () => navigation.navigate("LogIn");
   return (
-    <View>
-      <Text>Welcome</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
-        <View>
-          <Text>Go to Create Account</Text>
-        </View>
+    <Container>
+      <Logo resizeMode="contain" source={require("../assets/logo-white.png")} />
+      <TouchableOpacity onPress={goToCreateAccount}>
+        <CreateAccount>
+          <CreateAccountText>Create Account</CreateAccountText>
+        </CreateAccount>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("LogIn")}>
-        <View>
-          <Text>Go to Login</Text>
-        </View>
+      <TouchableOpacity onPress={goToLogIn}>
+        <LoginLink>Log In</LoginLink>
       </TouchableOpacity>
-    </View>
+    </Container>
   );
 }
