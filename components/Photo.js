a19c37e4811383258be8cfb91fc04b9d0446f39e
shin101 <kendras0127@gmail.com>
2023-08-03 13:25:36 +0900

    Infinite Scrolling part one

--- components/Photo.js
+++ components/Photo.js
@@ -15,7 +15,7 @@ const UserAvatar = styled.Image`
   margin-right: 10px;
   width: 25px;
   height: 25px;
-  border-radius: 50%;
+  border-radius: 12.5px;
 `;
 const Username = styled.Text`
   color: white;
