27b1b224ae4aaf3a0ed9ace829124dcd74112a23
shin101 <kendras0127@gmail.com>
2023-08-10 13:56:37 +0900

    search part three, doesn't fully function

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Text, TextInput, View } from "react-native";
+import { ActivityIndicator, Text, TextInput, View } from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 import { useForm } from "react-hook-form";
@@ -15,11 +15,25 @@ const SEARCH_PHOTOS = gql`
   }
 `;
 
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
-  const { setValue, register, watch } = useForm();
-  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
+  const { setValue, register, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+  const onValid = ({ keyword }) => {
+    startQueryFn({ variables: { keyword } });
+  };
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: "white" }}
@@ -30,25 +44,35 @@ export default function Search({ navigation }) {
       returnKeyType="Search"
       autoCorrect={false}
       onChangeText={(text) => setValue("keyword", text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
 
   useEffect(() => {
     navigation.setOptions({ headerTitle: SearchBox });
-    register("keyword");
+    register("keyword", { required: true, minLength: 3 });
   }, []);
 
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: "black",
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Photo</Text>
+      <View style={{ flex: 1, backgroundColor: "black" }}>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined &&
+        data?.searchPhotos?.length === 0 ? (
+          <MessageContainer>
+            <MessageText>could not find anything</MessageText>
+          </MessageContainer>
+        ) : null}
       </View>
     </DismissKeyboard>
   );
