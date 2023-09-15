eba6bd0d32e2574eb0549ef5fbbfe9aef6ed2a5f
shin101 <kendras0127@gmail.com>
2023-09-15 01:17:05 -0400

    Take photo part 1

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,11 +1,47 @@
-import React from "react";
+import { Camera } from "expo-camera";
+import React, { useState, useEffect } from "react";
+
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import { Text, View } from "react-native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Actions = styled.View`
+  flex: 0.35;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TakePhotoBtn = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 50px;
+  border: 1px solid rgba(255, 255, 255, 0.8);
+  opacity: 0.5;
+`;
 
 export default function TakePhoto() {
+  const [ok, setOk] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const getPermissions = async () => {
+    const permissions = await Camera.requestCameraPermissionsAsync();
+    setOk(granted);
+  };
+  useEffect(() => {
+    getPermissions();
+  }, []);
   return (
-    <View>
-      <Text>TakePhoto</Text>
-    </View>
+    <Container>
+      <Camera type={cameraType} style={{ flex: 1 }} />
+      <Actions>
+        <TakePhotoBtn></TakePhotoBtn>
+        <TouchableOpacity></TouchableOpacity>
+      </Actions>
+    </Container>
   );
 }
